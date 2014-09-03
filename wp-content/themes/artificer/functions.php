53dd86944a5c2da79a7a983c69267d7e1a7fac9e
Robert <bohdi5406@gmail.com>
2014-09-03 14:49:55 -0500

    browser check sans ie

--- wp-content/themes/artificer/functions.php
+++ wp-content/themes/artificer/functions.php
@@ -33,10 +33,86 @@ if ( is_woocommerce_activated() ) {
 /*-----------------------------------------------------------------------------------*/
 /* You can add custom functions below */
 /*-----------------------------------------------------------------------------------*/
+add_action( 'wp_enqueue_scripts', 'add_jquery' );
+add_action( 'wp_footer', '' );
+
 register_sidebar(array(
 'name'=> 'My Custom Widget Area',
 'id' => 'custom'
 ));
+
+function add_jquery()
+{
+	wp_enqueue_script( 'jquery' );
+}
+
+function touchMenu()
+{
+?>
+<script type="text/javascript">
+$jQuery(function()
+{
+    var menuActive = false,
+        touched = false,
+        $nav = $('.nav');
+
+    function removeActive(callback)
+    {
+        $nav.find('.MenuActive').removeClass('MenuActive');
+        callback();
+    }
+
+    function newActive($this,menu)
+    {
+        removeActive(function(){
+            $this.next().addClass('MenuActive').queue(function(){
+                if(menu){
+                    menuActive = true;
+                    touched = false;
+                } else {
+                    touched = true;
+                }
+            }).dequeue();
+        });   
+    }
+
+    $nav.on({
+        touchstart:function(e){            
+            e.stopPropagation();
+            newActive($(this),touched);
+        },
+        mouseenter:function(){
+            newActive($(this),true);  
+        },
+        click:function(e){
+            e.preventDefault();
+
+            if(menuActive){
+                $(this).trigger('trueClick',e.target);
+            }
+        },
+        trueClick:function(e,$target){
+            $(this).parents('.nav').trigger('mouseleave');
+            window.location.href = $target;
+        }
+    },'li .has_children').on('mouseleave',function(){
+        removeActive(function(){
+            menuActive = false;
+            touched = false;
+        });
+    });
+
+    $('html').on('touchstart',function(e)
+    {
+    	if(menuActive)
+    	{
+        $nav.trigger('mouseleave');
+    	}
+    });
+});
+</script>
+<?php 
+}
 /*-----------------------------------------------------------------------------------*/
 /* Don't add any code below here or the sky will fall down */
 /*-----------------------------------------------------------------------------------*/
